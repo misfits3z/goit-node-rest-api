c19ce288297a574599d70f1f881ea00b9f2b17c0
Didenko Victoria <155799784+misfits3z@users.noreply.github.com>
2025-06-09 17:28:12 +0800

    contactsServices fixed, created controllers

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -2,7 +2,10 @@ import fs from "fs/promises";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
 
-const contactsPath = path.resolve("src", "db", "contacts.json");
+const contactsPath = path.resolve("db", "contacts.json");
+
+const updateContact = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
 async function listContacts() {
   const data = await fs.readFile(contactsPath, "utf-8");
@@ -43,4 +46,21 @@ async function addContact(name, email, phone) {
   //  Повертає об'єкт доданого контакту (з id).
 }
 
-export { listContacts, getContactById, removeContact, addContact };
+async function updateContactById(id, data) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === id);
+  if (idx === -1) return null;
+  contacts[idx] = { ...contacts[idx], ...data };
+  await updateContact(contacts);
+  return contacts[idx];
+};
+
+export {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContactById,
+};
+  
+  
