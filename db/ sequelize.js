f1dae614f776fc333f876e2af044835baa8c8589
Didenko Victoria <155799784+misfits3z@users.noreply.github.com>
2025-06-16 10:51:58 +0800

    add sequelize, fixed app.js

---
+++ db/ sequelize.js
@@ -0,0 +1,29 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sequelize = new Sequelize({
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dialectOptions: {
+    ssl: process.env.DB_SSL === "true",
+  },
+});
+  
+
+try {
+    await sequelize.authenticate();
+    console.log("Database connection successful");
+    
+} catch (error) {
+    console.log("Failed connect database")
+    console.lo(error)
+    
+}
+
+export default sequelize;
---
+++ db/Contact.js
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize";
+import sequelize from "./ sequelize";
+
+const Contact = sequelize.define(
+    'contact', {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      favorite: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+    }
+)  
